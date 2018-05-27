0a1e435f902b15fcd8dfa5a56d08cc1bc9d5e7f3
Benjamin Sweney <benjamin@sweney.co>
2018-05-27 12:59:59 +1200

    Monitor

--- script/boot.py
+++ script/boot.py
@@ -1,4 +1,15 @@
 import pycom
-import time
+import machine
+from network import WLAN
 
 pycom.heartbeat(False)
+
+wlan = WLAN(mode=WLAN.STA)
+wlan.antenna(WLAN.INT_ANT)
+
+wlan.connect('iotakl24', auth=(WLAN.WPA2, 'iotakl17'), timeout=5000)
+
+while not wlan.isconnected():
+    machine.idle()
+
+print('Connected to Wifi')
--- script/main.py
+++ script/main.py
@@ -1,10 +1,36 @@
+import pycom
+import time
+from pysense import Pysense
 from LIS2HH12 import LIS2HH12
-from pytrack import Pytrack
-py = Pytrack()
-acc = LIS2HH12()
+from SI7006A20 import SI7006A20
+from LTR329ALS01 import LTR329ALS01
+from MPL3115A2 import MPL3115A2, ALTITUDE, PRESSURE
+
+py = Pysense()
+si = SI7006A20(py)
+lt = LTR329ALS01(py)
+li = LIS2HH12(py)
+
+def read_light():
+    colours = [0x7f0000, 0x007f00, 0x0000ff]
+
+    for colour in colours:
+        pycom.rgbled(colour) # toggle on rgb
+        time.sleep_ms(250)
+        reading = lt.light()
+        print(reading)
+        return reading # append light reading to lights
+
+def read_acceleromator():
+    reading = li.acceleration()
+    print(reading)
+    return reading # append light reading to lights
 
 while True:
-   pitch = acc.pitch()
-   roll = acc.roll()
-   print('{},{}'.format(pitch,roll))
-   time.sleep_ms(100)
+    light_readings = []
+    acceleromator_readings = []
+
+    light_readings.append(read_light())
+    acceleromator_readings.append(read_acceleromator())
+
+#os.mkfs('/flash')
